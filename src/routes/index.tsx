f413bef2e69361c01d8cf5c28acd9af1a8431cbc
Matheus Corrêa <mcorreadesign@gmail.com>
2021-10-15 16:25:55 -0300

    comp: routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Home from "pages/Home";
+import AssetDetails from "pages/AssetDetails";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const Routes: React.FC = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path={"/asset/:id"} exact component={AssetDetails} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
