f7a142141d9d2968c35f2de6e88a4ba82667c45a
Matheus Corrêa <mcorreadesign@gmail.com>
2021-10-17 22:00:00 -0300

    refat: not used imports

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import getData from "api/getData";
 import { Assets } from "interface/assets";
 
