0f2b18e582acb665471abd3d4fd8ce33bc5de11c
Matheus Corrêa <mcorreadesign@gmail.com>
2021-10-18 09:24:35 -0300

    fix: AssetDetails page size

--- src/pages/AssetDetails/styles.ts
+++ src/pages/AssetDetails/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   position: absolute;
   width: 100%;
-  height: 125%;
+  height: 100%;
   color: var(--alt_background);
 `;
 interface BannerImageProps {
